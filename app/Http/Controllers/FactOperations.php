9a21e91ec052a98e612100fddd68449a72a782b5
dhairya <dhairyasenjaliya@gmail.com>
2019-03-15 11:57:41 +0530

    test

--- app/Http/Controllers/FactOperations.php
+++ app/Http/Controllers/FactOperations.php
@@ -49,35 +49,11 @@ class FactOperations extends Controller
           ]);
           $Fact->save();
 
-          $optionBuilder = new OptionsBuilder();
-          $optionBuilder->setTimeToLive(60*20);
 
           $notificationBuilder = new PayloadNotificationBuilder('Latest Facts');
           $notificationBuilder->setBody('New Facts Added')->setSound('default');
-
-          $dataBuilder = new PayloadDataBuilder();
-          $dataBuilder->addData(['a_data' => 'my_data']);
-
-          $option = $optionBuilder->build();
-          $notification = $notificationBuilder->build();
-          $data = $dataBuilder->build();
-
-          $token = "a_registration_from_your_database";
-
-          $downstreamResponse = FCM::sendTo($token, $option, $notification, $data);
-
-          $downstreamResponse->numberSuccess();
-          $downstreamResponse->numberFailure();
-          $downstreamResponse->numberModification();
-
-          //return Array - you must remove all this tokens in your database
-          $downstreamResponse->tokensToDelete();
-
-          //return Array (key : oldToken, value : new token - you must change the token in your database )
-          $downstreamResponse->tokensToModify();
-
-          //return Array - you should try to resend the message to the tokens in the array
-          $downstreamResponse->tokensToRetry();
+          
+          dd($notificationBuilder);
 
           return redirect('/managefact')->with('success', 'Fact Added!!');
     }
